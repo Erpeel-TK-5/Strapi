958e3dc2080bbbdda79c40102bec95eaf6724a0a
Razita Afrina Nuriyan <razita.afrina@gmail.com>
2022-12-03 14:24:41 +0700

    Create data architecture & API

---
+++ src/api/calendar-user/controllers/calendar-user.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * calendar-user controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::calendar-user.calendar-user');
---
+++ src/api/calendar-user/routes/calendar-user.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * calendar-user router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::calendar-user.calendar-user');
